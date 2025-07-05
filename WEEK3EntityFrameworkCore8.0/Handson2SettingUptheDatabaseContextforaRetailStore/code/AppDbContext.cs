843d3f13c58e73493235cd1f9d44de92331911a9
iiishita05 <ishitasrivastava003@gmail.com>
2025-07-06 02:07:49 +0530

    week 3 hands on solutions

---
+++ WEEK3EntityFrameworkCore8.0/Handson2SettingUptheDatabaseContextforaRetailStore/code/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+public class AppDbContext : DbContext
+{
+    public DbSet<Product> Products { get; set; }
+    public DbSet<Category> Categories { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer(@"Server=localhost\SQLEXPRESS;Database=RetailDb;Trusted_Connection=True;Encrypt=False;");
+    }
+}
---
+++ WEEK3EntityFrameworkCore8.0/Handson2SettingUptheDatabaseContextforaRetailStore/code/Category.cs
@@ -0,0 +1,5 @@
+﻿using System.Collections.Generic;
+public class Category{
+    public int Id { get; set; }
+    public string Name { get; set; }
+ public List<Product> Products { get; set; }}
